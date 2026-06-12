2947d6c471f0937cbee50355943a556a72b2a672
Sanz98 <fonsecasanzd@gmail.com>
2026-06-12 11:47:09 -0300

    Finalizando projeto e configurando deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Presente-namorada/' // Deve ser o nome exato do seu repositório
+  base: '/Presente-namorada/' 
 })
